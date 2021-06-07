65327e7c5a6f6140eaa366e8a350c237653f084e
Ankur Paul <suryashi2013@gmail.com>
2021-06-07 22:40:49 +0530

    .

---
+++ Live Sessions/JavaScript/script.js
@@ -0,0 +1,36 @@
+/* var nums = [3, 7, -4];
+
+var count = 0;
+for (const num in nums) {
+	if (num < 0) {
+		++count;
+	}
+}
+console.log(count % 2 == 0 ? "-" : "+");
+ */
+
+/* var x = 0;
+var y = -1;
+var z = 4;
+if (x > y && x > z)
+	console.log(y > z ? x + ", " + y + ", " + z : x + ", " + z + ", " + y);
+else if (y > x && y > z)
+	console.log(x > z ? y + ", " + x + ", " + z : y + ", " + z + ", " + x);
+else if (z > x && z > y)
+	console.log(x > y ? z + ", " + x + ", " + y : z + ", " + y + ", " + x); */
+
+// for (var c = 1; c <= 50; ++c) console.log(c % 2 === 0 ? "Even" : "Odd");
+
+/* for (var c = 1; c <= 100; ++c) {
+	if (c % 3 === 0 && c % 5 === 0) console.log("upGrad");
+	else if (c % 3 === 0) console.log("up");
+	else if (c % 5 === 0) console.log("Grad");
+} */
+
+/* var sum = 0;
+for (var c = 1; c <= 1000; ++c) {
+	sum += c % 15 === 0 ? c : 0;
+} */
+
+/* var a = {};
+console.log(typeof a); */
