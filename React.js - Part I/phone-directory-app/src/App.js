d1bf73f7231f5e72ad15976d95d06a8df536e60c
nooobcoder <suryashi2013@gmail.com>
2021-06-29 05:17:59 +0000

    Create Skeleton of Home Page

--- React.js - Part I/phone-directory-app/src/App.js
+++ React.js - Part I/phone-directory-app/src/App.js
@@ -1,8 +1,26 @@
 import "./App.css";
-import React from "react";
+import React, { Fragment } from "react";
 
 function App() {
-	return <div className="App">Phone Directory</div>;
+	return (
+		<Fragment>
+			{/* <div className="App">Phone Directory</div>
+			<button>Add</button>
+			<div>
+				<span>Name</span>
+				<br />
+				<span>Phone</span>
+			</div> */}
+
+			<label htmlFor="name">Name</label>
+			<input
+				id="name"
+				type="text"
+				placeholder={"Type Here"}
+				defaultValue="Ankur Paul"
+			/>
+		</Fragment>
+	);
 }
 
 export default App;
