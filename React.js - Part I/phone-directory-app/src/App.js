1b6ca611daebbd7405dc5e498b37f45e4b20aa70
nooobcoder <suryashi2013@gmail.com>
2021-06-29 05:08:16 +0000

    Modified css and displayed app header

--- React.js - Part I/phone-directory-app/src/App.js
+++ React.js - Part I/phone-directory-app/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import React from "react";
 
 function App() {
-	return <div className="App"></div>;
+	return <div className="App">Phone Directory</div>;
 }
 
 export default App;
