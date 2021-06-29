6ea8585b8c94782f829f0d5333b224789ca307a8
nooobcoder <suryashi2013@gmail.com>
2021-06-29 09:48:09 +0000

    styling improvements

--- React.js - Part I/phone-directory-app/src/App.js
+++ React.js - Part I/phone-directory-app/src/App.js
@@ -3,15 +3,31 @@ import React, { Fragment } from "react";
 import Header from "./Header.jsx";
 
 function App() {
+	const contacts = [
+		{ id: 1, name: "Ankur Paul", phone: "8888888888" },
+		{ id: 2, name: "Joseph Pristine", phone: "7777777777" },
+	];
+
 	return (
 		<Fragment>
 			<Header />
 			<div className="container">
 				<button>Add</button>
-				<div>
-					<span>Name</span>
-					<span>Phone</span>
+				<div className="grid-container heading-container">
+					<span className="grid-item name-heading">
+						<h2>Name</h2>
+					</span>
+					<span className="grid-item phone-heading">
+						<h2>Phone</h2>
+					</span>
 				</div>
+				{contacts.map(({ id, name, phone }) => (
+					<div key={id} className="grid-container">
+						<span className="grid-item">{name}</span>
+						<span className="grid-item">{phone}</span>
+						<button className="delete-contact">Delete</button>
+					</div>
+				))}
 			</div>
 		</Fragment>
 	);
