04578c05aff7d2a206a461058cb6028f8038de47
nooobcoder <suryashi2013@gmail.com>
2021-07-07 09:42:45 +0530

    Added modal and it's controller

--- React.js - Part II/movies-app/src/components/Header.jsx
+++ React.js - Part II/movies-app/src/components/Header.jsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
+import Modal from "react-modal";
 
 import "../styles/Header.css";
 import Logo from "../assets/logo.svg";
 
 const Header = () => {
+	const [showModal, setModalVisibility] = useState(false);
+	console.log(showModal);
 	return (
 		<div className="app-header">
 			<img src={Logo} alt="app-logo" className="app-logo" />
-			<Button variant="contained" color="default" className="login-button">
+			<Button
+				variant="contained"
+				color="default"
+				className="login-button"
+				onClick={() => setModalVisibility((prevState) => !prevState)} // Toggling the modal visibility
+			>
 				Login
 			</Button>
+			{showModal && (
+				<Modal isOpen={showModal} contentLabel="login">
+					<p>This is the modal</p>
+				</Modal>
+			)}
 		</div>
 	);
 };
