18b84401cb3a11b604b0df3a3b3d858fb27aea2d
nooobcoder <suryashi2013@gmail.com>
2021-07-07 09:32:25 +0530

    Added Logo

--- React.js - Part II/movies-app/src/components/Header.jsx
+++ React.js - Part II/movies-app/src/components/Header.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 
+import "../styles/Header.css";
+import Logo from "../assets/logo.svg";
+
 const Header = () => {
 	return (
-		<div>
-			<Button variant="contained" color="default">
+		<div className="app-header">
+			<img src={Logo} alt="app-logo" className="app-logo" />
+			<Button variant="contained" color="default" className="login-button">
 				Login
 			</Button>
 		</div>
