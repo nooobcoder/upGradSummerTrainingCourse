90bfe3e3bbc8f8ea179ff68b03de8467d178e9ea
nooobcoder <suryashi2013@gmail.com>
2021-07-07 09:23:06 +0530

    Added login button on Header

---
+++ React.js - Part II/movies-app/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+
+const Header = () => {
+	return (
+		<div>
+			<Button variant="contained" color="default">
+				Login
+			</Button>
+		</div>
+	);
+};
+
+export default Header;
--- React.js - Part II/movies-app/src/components/Home.jsx
+++ React.js - Part II/movies-app/src/components/Home.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+
+// User defined imports
 import "../styles/Home.css";
+import Header from "./Header";
 
 const Home = () => {
-	return <div>Homepage</div>;
+	return (
+		<div>
+			<Header />
+		</div>
+	);
 };
 
 export default Home;
